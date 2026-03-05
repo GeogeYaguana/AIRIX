6a6fb1dac6da0001f17e4f3b1a52420979b6c369
george yaguana <sebyaguana@gmail.com>
2026-03-04 22:23:59 -0500

    fix: error reload some pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './assets/css/tailwind.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/AIRIX'>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>
 )
